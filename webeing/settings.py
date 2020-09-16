9f2f0d176535ac9f9ec171c80f316eb972b9a52b
Seungjo_Han <seungjohan@github.com>
2020-09-16 21:35:01 +0900

    landing css pre

--- webeing/settings.py
+++ webeing/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'webeing.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, 'templaes'],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
